b151242d8719be7dab39d30e18857bf8cbe8df6e
Branson Bailey <156610886+BransonBailey@users.noreply.github.com>
2025-03-04 11:40:48 -0500

    Update install_graylog.sh

--- Basic_Graylog_Installation/install_graylog.sh
+++ Basic_Graylog_Installation/install_graylog.sh
@@ -19,8 +19,8 @@ sudo timedatectl set-timezone UTC
 
 # --- Install MongoDB 7.0 ---
 echo "[+] Adding MongoDB repository..."
-curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /etc/apt/keyrings/mongodb-server-7.0.gpg --dearmor
-echo "deb [ signed-by=/etc/apt/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg -o /etc/apt/keyrings/mongodb-server-6.0.gpg --dearmor
+echo "deb [ signed-by=/etc/apt/keyrings/mongodb-server-6.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/6.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
 echo "[+] Installing MongoDB..."
 sudo apt update --allow-change-held-packages && sudo apt install -y mongodb-org
