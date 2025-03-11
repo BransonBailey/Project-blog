f33f0c717ced7719ff2a5629f8b93dc494aedd01
Branson Bailey <156610886+BransonBailey@users.noreply.github.com>
2025-03-11 08:21:03 -0400

    Update install_graylog.sh

--- Basic_Graylog_Installation/install_graylog.sh
+++ Basic_Graylog_Installation/install_graylog.sh
@@ -28,7 +28,7 @@ echo "[+] Adding MongoDB repository..."
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
 # --- Install MongoDB ---
 echo "[+] Updating package lists..."
